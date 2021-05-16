209a473360246b3dfd3a9bd95cac445333f28810
吉村雅人 <yoshimuramasato@yoshimuramasatonoMacBook-Air.local>
2021-05-16 17:37:28 +0900

    リアルシミュレーションで試し

--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Realsimulation
+from .models import Fandamental, Realsimulation
 from django.template.response import TemplateResponse
 from django.db.models import F
 
@@ -8,13 +8,14 @@ def index(request):
   return TemplateResponse(request,'test_app/index.html')
 
 def realsimulation(request):
-    stock = Realsimulation.objects.order_by("-date")[:12]
+    # stock = Realsimulation.objects.order_by("-date")[:12]
 
-    condition = {
-        'date':stock[0].date,
-        'close__gt':F('bb'),
-        'days150__gt':F('days200')
-    }
-    datas = Realsimulation.objects.all().filter(**condition)
+    # condition = {
+    #     'date':stock[0].date,
+    #     'close__gt':F('bb'),
+    #     'days150__gt':F('days200')
+    # }
+    # datas = Realsimulation.objects.all().filter(**condition)
+    datas = Fandamental.objects.all()
 
     return render(request,"test_app/real.html",{'datas': datas})
