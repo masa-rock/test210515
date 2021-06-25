addaa5dea1a99d288648974600f28e379b494ea6
吉村雅人 <yoshimuramasato@yoshimuramasatonoMacBook-Air.local>
2021-06-25 23:17:27 +0900

    herokuエラー回避

--- test_app/views.py
+++ test_app/views.py
@@ -55,46 +55,82 @@ def fandamental_all(request):
     return render(request,"test_app/fanda_all.html",{'pages':pages, 'counter':counter,})
 
 def fandamental_test1(request):
-    condition = {
-        'rieki_jousyou_0__gte':100,
-        'rieki_jousyou_1__gte':100,
-        'rieki_jousyou_2__gte':40,
+    jousyouritu = 5
+    condition ={
+        "s_er_0__gt" : (jousyouritu/100+1)*F("s_er_4"),
+        "s_er_1__gt" : (jousyouritu/100+1)*F("s_er_5"),
+        "s_er_2__gt" : (jousyouritu/100+1)*F("s_er_6"),
+        "s_er_3__gt" : (jousyouritu/100+1)*F("s_er_7"),
+        "s_er_4__gt" : (jousyouritu/100+1)*F("s_er_8"),
+        "s_er_5__gt" : (jousyouritu/100+1)*F("s_er_9"),
+        "s_er_6__gt" : (jousyouritu/100+1)*F("s_er_10"),
+        "s_er_7__gt" : (jousyouritu/100+1)*F("s_er_11"),
     }
-    datas = Fanda.objects.all().filter(**condition)
+    datas = Fanda.objects.filter(**condition)
     counter = datas.count()
-    return render(request,"test_app/fanda_test1.html",{'datas': datas,
-    'counter':counter
-    })
+    paginator = Paginator(datas, 20)
+    page = request.GET.get('page', 1)
+    try:
+    	pages = paginator.page(page)
+    except PageNotAnInteger:
+    	pages = paginator.page(1)
+    except EmptyPage:
+    	pages = paginator.page(1)
+    return render(request,"test_app/fanda_test1.html",{'pages':pages, 'counter':counter,})
 
 def fandamental_test2(request):
-    condition = {
-        'rieki_jousyou_0__lte':50,
-        'rieki_jousyou_1__lte':50,
-        'rieki_jousyou_2__lte':50,
-        'rieki_jousyou_3__gte':50,
+    jousyouritu = 10
+    condition ={
+        "y_u_0__gt" : (jousyouritu/100+1)*F("y_u_1"),
+        "y_u_1__gt" : (jousyouritu/100+1)*F("y_u_2"),
+        "y_u_2__gt" : (jousyouritu/100+1)*F("y_u_3"),
+        "y_u_3__gt" : (jousyouritu/100+1)*F("y_u_4"),
+        "y_u_4__gt" : (jousyouritu/100+1)*F("y_u_5"),
+        "y_u_5__gt" : (jousyouritu/100+1)*F("y_u_6"),
+        "y_u_6__gt" : (jousyouritu/100+1)*F("y_u_7"),
+        "y_u_7__gt" : (jousyouritu/100+1)*F("y_u_8"),
+        "y_u_8__gt" : (jousyouritu/100+1)*F("y_u_9"),
+        "y_u_9__gt" : (jousyouritu/100+1)*F("y_u_10"),
     }
-    datas = Fanda.objects.all().filter(**condition)
+    datas = Fanda.objects.filter(**condition)
     counter = datas.count()
-    return render(request,"test_app/fanda_test2.html",{'datas': datas,
-    'counter':counter
-    })
+    paginator = Paginator(datas, 20)
+    page = request.GET.get('page', 1)
+    try:
+    	pages = paginator.page(page)
+    except PageNotAnInteger:
+    	pages = paginator.page(1)
+    except EmptyPage:
+    	pages = paginator.page(1)
+
+    return render(request,"test_app/fanda_test2.html",{'pages':pages, 'counter':counter,})
 
 def fandamental_test3(request):
-    condition = {
-        'rieki_jousyou_0__gte':100,
-        'rieki_jousyou_1__gte':100,
-        'rieki_jousyou_2__gte':40,
-        'uriage_jousyou_0__gte':0,
-        'uriage_jousyou_1__gte':0,
-        'uriage_jousyou_2__gte':0,
+    jousyouritu = 10
+    condition ={
+        "y_er_0__gt" : (jousyouritu/100+1)*F("y_er_1"),
+        "y_er_1__gt" : (jousyouritu/100+1)*F("y_er_2"),
+        "y_er_2__gt" : (jousyouritu/100+1)*F("y_er_3"),
+        "y_er_3__gt" : (jousyouritu/100+1)*F("y_er_4"),
+        "y_er_4__gt" : (jousyouritu/100+1)*F("y_er_5"),
+        "y_er_5__gt" : (jousyouritu/100+1)*F("y_er_6"),
+        "y_er_6__gt" : (jousyouritu/100+1)*F("y_er_7"),
+        "y_er_7__gt" : (jousyouritu/100+1)*F("y_er_8"),
+        "y_er_8__gt" : (jousyouritu/100+1)*F("y_er_9"),
+        "y_er_9__gt" : (jousyouritu/100+1)*F("y_er_10"),
     }
-    # Input_Form =InputForm()
-    datas = Fanda.objects.all().filter(**condition)
+    datas = Fanda.objects.filter(**condition)
     counter = datas.count()
-    return render(request,"test_app/fanda_test3.html",{'datas': datas,
-    'counter':counter,
-    # 'Input_Form':Input_Form,
-        })
+    paginator = Paginator(datas, 20)
+    page = request.GET.get('page', 1)
+    try:
+    	pages = paginator.page(page)
+    except PageNotAnInteger:
+    	pages = paginator.page(1)
+    except EmptyPage:
+    	pages = paginator.page(1)
+
+    return render(request,"test_app/fanda_test3.html",{'pages':pages, 'counter':counter,})
 
 def real_result(request):
     datas = Realsimulation_result_v1.objects.all()
